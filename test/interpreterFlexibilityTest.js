1a37dc8eeb95818e29b19cc1ba5c348205420af1
Marcos <marcospernica@gmail.com>
2017-10-08 20:16:20 -0300

    Agregados test de flexibilidad en la escritura de la base de datos/consultas

---
+++ test/interpreterFlexibilityTest.js
@@ -0,0 +1,92 @@
+var expect = require("chai").expect;
+var should = require('should');
+var assert = require('assert');
+
+var Interpreter = require('../src/interpreter');
+
+
+
+
+describe("InterpreterFlexibilityTest", function () {
+
+    var db = [
+        "varon(juan      )",
+        "varon(      pepe 				 	 		  		 	 	 	 ).",
+        "varon(hector).",
+        "varon(roberto).",
+        "varon(alejandro).",
+        "mujer(maria).",
+        "mujer(cecilia).",
+        "padre(juan, pepe).",
+        "padre(juan, 			 	 	 	 pepa).",
+        "padre(hector,maria).",
+        "padre(roberto, alejandro).",
+        "padre(roberto, cecilia).",
+        "hijo(X, Y) :- varon(X), padre(Y, X).",
+        "hija(X, Y) :- mujer(X), padre(Y, X)."
+    ];
+
+   var interpreter = null;
+
+    before(function () {
+        // runs before all tests in this block
+    });
+
+    after(function () {
+        // runs after all tests in this block
+    });
+
+    beforeEach(function () {
+        // runs before each test in this block
+        interpreter = new Interpreter();
+        interpreter.parseDB(db);
+    });
+
+    afterEach(function () {
+        // runs after each test in this block
+    });
+
+
+    describe('Interpreter Facts', function () {
+
+        it('varon(juan) should be true', function () {
+            assert(interpreter.checkQuery('varon(juan)'));
+        });
+
+        it('varon(maria) should be false', function () {
+            assert(interpreter.checkQuery('varon(maria)') === false);
+        });
+
+        it('mujer(cecilia) should be true', function () {
+            assert(interpreter.checkQuery('mujer(cecilia)'));
+        });
+
+        it('padre(juan, pepe) should be true', function () {
+            assert(interpreter.checkQuery('padre(juan, pepe)') === true);
+        });
+
+        it('padre(mario, pepe) should be false', function () {
+            assert(interpreter.checkQuery('padre(mario, pepe)') === false);
+        });
+
+   
+
+    });
+
+    describe('Interpreter Rules', function () {
+
+        it('hijo(pepe, juan) should be true', function () {
+            assert(interpreter.checkQuery('hijo(pepe, juan)') === true);
+        });
+        it('hija(maria, roberto) should be false', function () {
+            assert(interpreter.checkQuery('hija(maria, roberto)') === false);
+        });
+        it('hijo(pepe, juan) should be true', function () {
+            assert(interpreter.checkQuery('hijo(pepe, juan)'));
+        });
+
+       
+
+    });
+
+});
